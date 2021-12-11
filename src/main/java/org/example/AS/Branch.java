10656129744fa8ed1b25adeb8703ade8b9198902
Tisu01 <mail.artur@tlen.pl>
2021-12-11 14:28:36 +0100

    Dodanie klas i stworzenie metod

---
+++ src/main/java/org/example/AS/Branch.java
@@ -0,0 +1,19 @@
+package org.example.AS;
+
+public class Branch {
+    private int idAdress;
+    private String name;
+    private int code;
+
+    public Branch(int idAdress, String name, int code) {
+        this.idAdress = idAdress;
+        this.name = name;
+        this.code = code;
+    }
+
+    public void receivetheparcel() {
+    }
+    public void sendtheparcel() {
+    }
+}
+

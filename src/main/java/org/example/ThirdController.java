a7f24f5799faa914191fd3970128bdd3675ed6c6
Tisu01 <mail.artur@tlen.pl>
2022-01-11 19:47:23 +0100

    Add GUI scene

---
+++ src/main/java/org/example/ThirdController.java
@@ -0,0 +1,28 @@
+package org.example;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class Login {
+
+    @FXML
+    void btnCreateanaccountClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnLoginClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnUserClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnWorkerClicked(ActionEvent event) {
+
+    }
+
+}
